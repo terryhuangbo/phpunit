cda0cfcbbb0d581b403eca4b454e84aed50f06e8
Huangbo <huangbo@yunshan.net.cn>
2017-04-18 18:18:21 +0800

    incomplete and skip

--- HbTest.php
+++ HbTest.php
@@ -1,29 +1,64 @@
 <?php
-require(__DIR__ . '/vendor/autoload.php');
+
+require __DIR__.'/vendor/autoload.php';
 
 use PHPUnit\Framework\TestCase;
 
-class HbTest extends TestCase
+class StackTest extends TestCase
 {
+    protected $stack;
+
+    protected function setUp()
+    {
+        $this->stack = [];
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    public static function setUpBeforeClass() //该test类的第一个测试运行前调用
+    {
+    }
+
+    public static function tearDownAfterClass() //该test类的最后一个测试运行后调用
+    {
+    }
+
+    public function testEmpty()
+    {
+        $this->assertTrue(empty($this->stack));
+    }
+
+    public function testPush()
+    {
+        array_push($this->stack, 'foo');
+        $this->assertEquals('foo', $this->stack[count($this->stack) - 1]);
+        $this->assertFalse(empty($this->stack));
+    }
+
+    public function testPop()
+    {
+        array_push($this->stack, 'foo');
+        $this->assertEquals('foo', array_pop($this->stack));
+        $this->assertTrue(empty($this->stack));
+    }
+
+    public function testCover()
+    {
+        $this->markTestIncomplete('it is not fulfilled');
+    }
+
+    public function testVarDump()
+    {
+        var_dump(12);
+    }
+
     /**
-     * @author   hb
-     * @dataProvider additionProvider
+     * @covers
      */
-    public function testAdd($a, $b, $expected)
+    public function testUncover()
     {
-        echo 121;
-        $this->assertEquals($expected, $a + $b);
+        $this->markTestSkipped('it is skipped');
     }
-
-    public function additionProvider()
-    {
-        return [
-            'adding zeros'  => [0, 0, 0],
-            'one plus one'  => [1, 1, 3],
-            'zero plus one' => [0, 1, 1],
-            'one plus zero' => [1, 0, 1],
-        ];
-    }
-
 }
-
--- index.php
+++ index.php
@@ -1,23 +1,5 @@
 <?php
-require(__DIR__ . '/vendor/autoload.php');
 
-use PHPUnit\Framework\TestCase;
+require __DIR__.'/vendor/autoload.php';
 
-class StackTest extends TestCase
-{
-    public function testPushAndPop()
-    {
-        $stack = [];
-        $this->assertEquals(0, count($stack));
-
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack)-1]);
-        $this->assertEquals(1, count($stack));
-
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
-    }
-}
-
-$test = new StackTest();
-var_dump($test->testPushAndPop());
+phpinfo();
